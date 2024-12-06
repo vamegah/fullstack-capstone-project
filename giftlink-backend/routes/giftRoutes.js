acadfb6fd9f531bc6a7d9ee6b07c87ced28b2c31
vamegah <makvim@yahoo.com>
2024-12-06 16:29:10 -0500

    Added code to app.js db.js and giftRoutes.js

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -28,7 +28,7 @@ router.get('/:id', async (req, res) => {
         const id = req.params.id;
 
         // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        const gift = await collection.findOne({ "id": id });
+        const gift = await collection.findOne({ id: id });
 
         if (!gift) {
             return res.status(404).send('Gift not found');
