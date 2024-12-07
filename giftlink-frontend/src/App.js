38a8e1a201ea49e5c395a302a5561665c2f280bf
vamegah <102638632+vamegah@users.noreply.github.com>
2024-12-06 23:40:21 -0600

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import Profile from './components/Profile/Profile';
+import SearchPage from './components/SearchPage/SearchPage';
 function App() {
   const navigate = useNavigate();
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path="/app/search" element={<SearchPage />} />
           <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
